30c971c2ab40a9a369594b11a045b059d7b53e89
Jana <97198479+Janabdelgawad@users.noreply.github.com>
2023-09-23 13:12:29 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Card(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <img src={props.img} alt="avatar_img" />
+      <p>{props.tel}</p>
+      <p>{props.email}</p>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <div>
+    <h1>My Contacts</h1>
+    <Card
+      name="Chuck Norris"
+      img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+      tel="+918 372 574"
+      email="gmail@chucknorris.com"
+    />
+  </div>,
+  document.getElementById("root")
+);
